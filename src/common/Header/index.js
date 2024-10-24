268175d480a8c0ba5b2789c16380bd24092e22e7
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-10-24 17:39:50 +0200

    added Header Component

---
+++ src/common/Header/index.js
@@ -0,0 +1,15 @@
+import { StyledHeader, Title, List, Item } from "./styled";
+export const Header = () => (
+  <StyledHeader>
+    <Title>Shop-Spot</Title>
+    <nav>
+      <List>
+        <Item>Home</Item>
+        <Item>Electronics</Item>
+        <Item>Jewelery</Item>
+        <Item>men's clothing</Item>
+        <Item>women's clothing</Item>
+      </List>
+    </nav>
+  </StyledHeader>
+);
---
+++ src/common/Header/styled.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.header`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+`;
+
+export const Title = styled.h1`
+  font-size: 32px;
+`;
+
+export const List = styled.ul`
+  display: flex;
+  font-size: 16px;
+  list-style-type: none;
+`;
+
+export const Item = styled.li`
+  width: 140px;
+`;
