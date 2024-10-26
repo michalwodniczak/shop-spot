382a19d82fd39aee6928731a66a2eaf4fb0ab1ef
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-10-26 13:40:40 +0200

    changed max-width and added media query

--- src/common/Navigation/styled.js
+++ src/common/Navigation/styled.js
@@ -2,9 +2,12 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
   margin: 0 auto;
-  max-width: 1000px;
-  padding:10px;
+  max-width: 1200px;
   display: flex;
-  align-items:center;
+  align-items: center;
   justify-content: space-evenly;
+  
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+    justify-content: space-around;
+  }
 `;
