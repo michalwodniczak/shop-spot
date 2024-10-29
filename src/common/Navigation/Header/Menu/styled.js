b5e37e58c08ed130ceaac9c14474a7851278047a
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-10-29 17:33:21 +0100

    added logic to render mobile menu

--- src/common/Navigation/Header/Menu/styled.js
+++ src/common/Navigation/Header/Menu/styled.js
@@ -1,25 +1,43 @@
-import styled from "styled-components";
-import { NavLink } from "react-router-dom";
+import styled, { css } from "styled-components";
+import { NavLink, redirect } from "react-router-dom";
+
+export const Overlay = styled.div`
+  background-color: ${({ theme }) => theme.colors.gray};
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  z-index: 1;
+  display: ${({ isOpen }) => (isOpen ? "block" : "none")};
+  opacity: 0.9;
+`;
 
 export const List = styled.ul`
   display: flex;
-  font-size: 16px;
   list-style-type: none;
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
-    order: 1;
-  }
+  ${(props) =>
+    props.mobile &&
+    css`
+      flex-direction: column;
+    `}
 `;
 
 export const Item = styled.li`
   width: 140px;
-  text-align: center;
-  /* @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px){
-    display: none;
-  } */
+  margin: 0 auto;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+    font-size: 20px;
+    margin-top: 20px;
+    width: 200px;
+  }
 `;
 
 export const StyledNavLink = styled(NavLink)`
   text-decoration: none;
   color: ${({ theme }) => theme.colors.black};
+
+  &:hover {
+    color: blue;
+  }
 `;
