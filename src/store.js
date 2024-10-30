9db2685c78a74c9a4d84e42386c93b5c117d3a07
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-10-30 18:37:20 +0100

    added store

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productsReducer from "././features/Products/productsSlice";
 
-export default store = configureStore({
+const store = configureStore({
   reducer: {
     products: productsReducer,
   },
 });
+
+export default store;
