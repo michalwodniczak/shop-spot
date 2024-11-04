456064378c7c81322733c088585ad0bf80105650
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-11-04 17:18:13 +0100

    added middleware to Store

--- src/store.js
+++ src/store.js
@@ -1,10 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
+import createSagaMiddleware from "redux-saga";
+import { watchFetchProducts } from "./features/Products/productsSaga";
 import productsReducer from "././features/Products/productsSlice";
+
+const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
   reducer: {
     products: productsReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(sagaMiddleware),
 });
+
+sagaMiddleware.run(watchFetchProducts);
 
 export default store;
