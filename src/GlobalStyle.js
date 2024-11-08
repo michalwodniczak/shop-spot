1eaede816a412950b29d091f3d63b54c98c43648
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-11-08 16:55:30 +0100

    added prefix

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -2,14 +2,14 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
     html {
-		box-sizing:border-box;
-	}
+		box-sizing: border-box;
+	};
 
 	*,::after,::before {
-		box-sizing:inherit;
+		box-sizing: inherit;
     };
 
 	body {
 		font-family: "Hind Siliguri", sans-serif;
     };
-`
+`;
