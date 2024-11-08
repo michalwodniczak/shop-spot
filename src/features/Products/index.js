5361d9be280a4d1c1ed122143c4225f2327c2c99
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-11-08 17:46:23 +0100

    added LargeListWrapper to Products Component

--- src/features/Products/index.js
+++ src/features/Products/index.js
@@ -2,6 +2,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { Main } from "../../common/Main/Main";
 import { fetchProducts, selectProducts } from "./productsSlice";
 import { useEffect } from "react";
+import { LargeListWrapper } from "../../common/Tile/styled";
+import { ListLargeTile } from "../../common/Tile";
 
 export const Products = () => {
   const dispatch = useDispatch();
@@ -13,12 +15,19 @@ export const Products = () => {
 
   return (
     <Main>
-      {products.map((product) => (
-        <div key={product.id}>
-          {product.title}
-          <img src={product.image} width="200" />
-        </div>
-      ))}
+      <LargeListWrapper>
+        {products.map((product) => (
+          <li key={product.id}>
+            <ListLargeTile
+              image={product.image}
+              name={product.title}
+              rate={product.rating.rate}
+              countRate={product.rating.count}
+              price={product.price}
+            />
+          </li>
+        ))}
+      </LargeListWrapper>
     </Main>
   );
 };
