3cc0e09e5e2e393ed04dfc6cac370c73ce85b4a4
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-11-13 16:49:55 +0100

    added media query and changed value  of property in styled

--- src/common/Tile/styled.js
+++ src/common/Tile/styled.js
@@ -1,23 +1,33 @@
 import styled from "styled-components";
 
 export const LargeListWrapper = styled.ul`
-  width: 1000px;
-  margin: 0 auto;
   list-style-type: none;
   display: grid;
   grid-template-columns: repeat(3, 1fr);
+  justify-content: center;
   row-gap: 20px;
+  margin: 0 auto;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+    width: 100%;
+    grid-template-columns: 1fr;
+    row-gap: 10px;
+    padding:34px;
+  }
 `;
 
 export const Tile = styled.article`
-  width: 300px;
+  max-width: 360px;
   height: 400px;
   border: 1px solid black;
   border-radius: 5px;
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+    width: 100%;
+  }
 `;
 
 export const TileImageContainer = styled.div`
@@ -25,32 +35,37 @@ export const TileImageContainer = styled.div`
   height: 150px;
   display: flex;
   overflow: hidden;
-  margin: 20px 0;
+  margin: 20px auto;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+    width: 100%;
+    height: 100px;
+  }
 `;
 
 export const TileImage = styled.img`
   width: 100%;
   height: 100%;
   object-fit: contain;
-  padding: 4px;
 `;
 
 export const TileTextContainer = styled.div`
   width: 300px;
   display: flex;
   flex-direction: column;
+  padding: 10px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+    width: 100%;
+  }
 `;
 
 export const TileTitle = styled.h2`
   font-weight: 700;
   font-size: 16px;
-  text-align: left;
-  padding: 10px;
 `;
 
 export const TilePrice = styled.p`
   font-weight: 700;
   font-size: 20px;
-  padding: 20px;
-  align-self: flex-start;
 `;
