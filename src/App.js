1937fa8ae5f74537794ddcf6ae2a233530072638
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-12-16 22:41:36 +0100

    add Route to Cart

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import { Navigation } from "./common/Navigation";
 import { Products } from "./features/Products";
 import { Product } from "./features/Product";
 import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
+import { Cart } from "./features/Cart";
 
 function App() {
   return (
     <HashRouter>
       <Navigation />
       <Switch>
+        <Route path="/cart">
+          <Cart />
+        </Route>
         <Route path="/products/:id">
           <Product />
         </Route>
