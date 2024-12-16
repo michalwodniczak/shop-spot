aa9cb8b66a1209ac159a29f0eaf2d7981bdf3a34
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-12-16 22:41:48 +0100

    added Component Button

--- src/common/Button/index.js
+++ src/common/Button/index.js
@@ -1,7 +1,16 @@
+import { useDispatch } from "react-redux";
 import { StyledButton } from "./styled";
+import { addToCart } from "../../features/Product/productSlice";
 
-export const Button = () =>{
-    return(
-        <StyledButton>Add to Cart</StyledButton>
-    )
-}
+export const Button = ({ product }) => {
+  const dispatch = useDispatch();
+
+  const handleAddToCart = () => {
+    if (product && product.id) {
+      dispatch(addToCart(product));
+    } else {
+      console.error("produkt nie zostałdodany");
+    }
+  };
+  return <StyledButton onClick={handleAddToCart}>Add to Cart</StyledButton>;
+};
