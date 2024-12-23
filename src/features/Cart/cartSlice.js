35758de4237273c756859f467fc107ab0ab17be0
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-12-23 15:52:13 +0100

    added removeFromCart

--- src/features/Cart/cartSlice.js
+++ src/features/Cart/cartSlice.js
@@ -13,11 +13,17 @@ const cartSlice = createSlice({
         state.cart.push(product);
       }
     },
+    removeFromCart: (state, { payload: product }) => {
+      const isInCart = state.cart.some((item) => item.id === product.id);
+      if (isInCart) {
+        state.cart.shift(product);
+      }
+    },
   },
 });
 
 export const selectCart = (state) => state.cart.cart;
 
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, removeFromCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
--- src/features/Cart/index.js
+++ src/features/Cart/index.js
@@ -1,29 +1,33 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Main } from "../../common/Main/Main";
-import { selectCart } from "./cartSlice";
+import { removeFromCart, selectCart } from "./cartSlice";
 import { CartListWrapper } from "../../common/Tile/styled";
-import { CartTile } from "../../common/Tile";
+import { CartsTile } from "../../common/Tile";
+import { StyledButton } from "../../common/Button/styled";
 
 export const Cart = () => {
   const cartProducts = useSelector(selectCart);
+  const dispatch = useDispatch();
 
   return (
     <Main>
       <CartListWrapper>
         {cartProducts.length === 0 ? (
-          <p>Koszyk jest pusty!</p>
+          <CartsTile>
+            <p>Koszyk jest pusty!</p>
+          </CartsTile>
         ) : (
           cartProducts.map((product) => (
-            // <div key={product.id}>
-            //   <p>{product.title}</p>
-            //   <p>{product.price}</p>
-            //   <img src={product.image} />
-            // </div>
-            <CartTile key={product.id}
-            name={product.title}
-            image={product.image}
-            price={product.price}
-            />
+            <CartsTile
+              key={product.id}
+              name={product.title}
+              image={product.image}
+              price={product.price}
+            >
+              <StyledButton onClick={() => dispatch(removeFromCart(product))}>
+                Remove to Cart
+              </StyledButton>
+            </CartsTile>
           ))
         )}
       </CartListWrapper>
