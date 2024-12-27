db5f7c0da08a5e88be79df07e041f3c44d419dde
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-12-27 12:13:40 +0100

    added count Cart item

--- src/common/Navigation/CartButton/index.js
+++ src/common/Navigation/CartButton/index.js
@@ -1,12 +1,20 @@
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectCart } from "../../../features/Cart/cartSlice";
 import CartIcon from "../../../assets/cart.png";
-import { Button } from "./styled";
+import { Button, Text, BadgeWrapper } from "./styled";
 
 export const CartButton = () => {
+  const cartItem = useSelector(selectCart);
   return (
     <Link to="/cart">
       <Button>
         <img src={CartIcon} alt="" />
+        {cartItem.length > 0 && (
+          <BadgeWrapper>
+            <Text>{cartItem.length}</Text>
+          </BadgeWrapper>
+        )}
       </Button>
     </Link>
   );
--- src/common/Navigation/CartButton/styled.js
+++ src/common/Navigation/CartButton/styled.js
@@ -1,7 +1,28 @@
 import styled from "styled-components";
 
 export const Button = styled.button`
-    background-color: transparent;
-    border: none;
-    cursor: pointer;
-`
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  position: relative;
+`;
+
+export const BadgeWrapper = styled.div`
+  position: absolute;
+  top: 14px;
+  right: -4px;
+`;
+
+export const Text = styled.p`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: red;
+  border-radius: 50%;
+  color: ${({ theme }) => theme.colors.white};
+  font-weight: bold;
+  width: 18px;
+  height: 18px;
+  font-size: 16px;
+  margin: 0;
+`;
