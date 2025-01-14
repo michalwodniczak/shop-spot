f1421f91c0e5f36dac03d405b9bf3fe349ee8590
Michał Wodniczak <michal.wodniczak@o2.pl>
2025-01-14 16:47:22 +0100

    formatted code

--- src/features/Products/productsSaga.js
+++ src/features/Products/productsSaga.js
@@ -1,13 +1,17 @@
 import { call, put, takeLatest } from "redux-saga/effects";
 import { getProducts } from "../../getProducts";
-import { fetchProducts, fetchProductsSuccess, setStatus } from "./productsSlice";
+import {
+  fetchProducts,
+  fetchProductsSuccess,
+  setStatus
+} from "./productsSlice";
 
 function* fetchProductsHandler() {
   try {
     yield put(setStatus("loading"));
     const products = yield call(getProducts);
     yield put(fetchProductsSuccess(products));
-    yield put(setStatus("success"))
+    yield put(setStatus("success"));
   } catch {
     yield call(alert, "coś poszło nie tak");
   }
