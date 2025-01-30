aea5e647bdad1cc04aad050ae97e380395bfcd32
Michał Wodniczak <michal.wodniczak@o2.pl>
2025-01-30 16:40:20 +0100

    formatted code

--- src/features/Cart/cartSaga.js
+++ src/features/Cart/cartSaga.js
@@ -1,11 +1,10 @@
 import { call, takeEvery, select } from "redux-saga/effects";
-import { selectCart } from "./cartSlice";
+import { selectCart, addToCart, removeFromCart } from "./cartSlice";
 import { saveItemInLocalStorage } from "./getItem";
-import { addToCart, removeFromCart } from "./cartSlice";
 
 function* saveItemInLocalStorageHandler() {
   const cart = yield select(selectCart);
-  yield call(saveItemInLocalStorage, cart, 15);
+  yield call(saveItemInLocalStorage, cart);
 }
 
 export function* watchFetchCartItem() {
