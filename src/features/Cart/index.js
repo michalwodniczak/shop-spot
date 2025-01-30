af9c993e0a6437f3213f0c43a20cb8b5cae5c792
Michał Wodniczak <michal.wodniczak@o2.pl>
2025-01-30 16:45:33 +0100

    added logic to show the time until the cart is removed

--- src/features/Cart/index.js
+++ src/features/Cart/index.js
@@ -1,20 +1,59 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Main } from "../../common/Main/Main";
-import { removeFromCart, selectCart } from "./cartSlice";
-import { CartListWrapper} from "../../common/Tile/styled";
+import {
+  removeFromCart,
+  selectCart,
+  removeAllProductFromCart,
+} from "./cartSlice";
+import { CartListWrapper } from "../../common/Tile/styled";
 import { CartsTile } from "../../common/Tile";
 import { RemoveButton } from "../../common/Button/styled";
 import { CartText } from "./styled";
 
 export const Cart = () => {
-  const cartProducts = useSelector(selectCart);
   const dispatch = useDispatch();
-  
+  const cartProducts = useSelector(selectCart);
+  const [timeLeft, setTimeLeft] = useState(() => {
+    const storeData = JSON.parse(localStorage.getItem("cart"));
+    if (storeData?.expirationTime) {
+      const remainingTime = Math.floor(
+        (storeData.expirationTime - Date.now()) / 1000
+      );
+      return remainingTime > 0 ? remainingTime : 0;
+    }
+    return 0;
+  });
+
+  useEffect(() => {
+    if (timeLeft <= 0) {
+      localStorage.removeItem("cart");
+      dispatch(removeAllProductFromCart());
+      return;
+    }
+
+    const interval = setInterval(() => {
+      setTimeLeft((prev) => (prev > 0 ? prev - 1 : 0));
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [timeLeft, dispatch]);
+
+  const minutes = Math.floor(timeLeft / 60);
+  const seconds = timeLeft % 60;
+
   return (
     <Main>
+      {timeLeft > 0 ? (
+        <p>
+          Pozostały czas {minutes ? minutes : null} minut {seconds} sekund
+        </p>
+      ) : (
+        ""
+      )}
       <CartListWrapper>
         {cartProducts.length === 0 ? (
-            <CartText>Cart is empty!</CartText>
+          <CartText>Cart is empty!</CartText>
         ) : (
           cartProducts.map((product) => (
             <CartsTile
