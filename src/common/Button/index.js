b4608c75cd83ecfdd04754b720d16c1fb064ffc3
Michał Wodniczak <michal.wodniczak@o2.pl>
2025-01-30 16:39:35 +0100

    formatted code

--- src/common/Button/index.js
+++ src/common/Button/index.js
@@ -4,19 +4,18 @@ import { addToCart } from "../../features/Cart/cartSlice";
 
 export const Button = ({ product }) => {
   const dispatch = useDispatch();
- 
+
   const handleAddToCart = () => {
     const cartProduct = {
       id: product.id,
       title: product.title,
       price: product.price,
       image: product.image,
-    }
-    
+    };
+
     if (cartProduct && cartProduct.id) {
       dispatch(addToCart(cartProduct));
-    } 
-    
+    }
   };
   return <StyledButton onClick={handleAddToCart}>Add to Cart</StyledButton>;
 };
