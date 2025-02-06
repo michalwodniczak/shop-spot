b3ac72b6869dc0352852e74010a655b36fb07127
Michał Wodniczak <michal.wodniczak@o2.pl>
2025-02-06 16:53:35 +0100

    added style

--- src/features/Cart/CartTimer/styled.js
+++ src/features/Cart/CartTimer/styled.js
@@ -9,10 +9,25 @@ export const Wrapper = styled.div`
   position: fixed;
   bottom: 20px;
   right: 20px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mediumDevice}px) {
+    position: relative;
+    width: calc(100% - 20px);
+    margin: 20px auto;
+    padding: 2px;
+    bottom: 0;
+    right: 0;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.smallDevice}px) {
+    margin: 0 auto;
+  }
 `;
 
 export const Text = styled.p`
-    font-size: 14px;
-    font-weight: 700;
-    text-align: center;
-`
+  font-size: 14px;
+  font-weight: 700;
+  text-align: center;
+  display: inline;
+  margin: 0 2px;
+`;
