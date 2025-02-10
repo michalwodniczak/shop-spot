8d7ec77202d4284f6eb2921cc375c8ff6d54894b
Michał Wodniczak <michal.wodniczak@o2.pl>
2025-02-10 17:23:36 +0100

    added Nav

--- src/common/Navigation/Header/Menu/index.js
+++ src/common/Navigation/Header/Menu/index.js
@@ -3,6 +3,8 @@ import { useTheme } from "styled-components";
 import { Button } from "../../CartButton/styled";
 import { Item, List, StyledNavLink, Overlay } from "./styled";
 import hamburgerIcon from "../../../../assets/hamburgerMenu.png";
+import { Nav } from "../styled";
+
 
 export const Menu = () => {
   const theme = useTheme();
@@ -25,29 +27,32 @@ export const Menu = () => {
   };
 
   return (
-    <nav>
+    <Nav>
       {isMobile ? (
         <>
           <Button onClick={toggleMenu} aria-label="Toggle menu">
             <img src={hamburgerIcon} alt="Menu Icon" />
           </Button>
           <Overlay isOpen={isMenuOpen}>
+          <Button onClick={toggleMenu} aria-label="Toggle menu">
+            <img src={hamburgerIcon} alt="Menu Icon" />
+          </Button>
             <List mobile>
               <Item>
-                <StyledNavLink to="/category/electronics">
+                <StyledNavLink to="/category/electronics" onClick={()=> setIsMenuOpen(false)}>
                   Electronics
                 </StyledNavLink>
               </Item>
               <Item>
-                <StyledNavLink to="/category/jewelery">Jewelery</StyledNavLink>
+                <StyledNavLink to="/category/jewelery" onClick={()=> setIsMenuOpen(false)}>Jewelery</StyledNavLink>
               </Item>
               <Item>
-                <StyledNavLink to="/category/men's clothing">
+                <StyledNavLink to="/category/men's clothing" onClick={()=> setIsMenuOpen(false)}>
                   Men's clothing
                 </StyledNavLink>
               </Item>
               <Item>
-                <StyledNavLink to="/category/women's clothing">
+                <StyledNavLink to="/category/women's clothing" onClick={()=> setIsMenuOpen(false)}>
                   Women's clothing
                 </StyledNavLink>
               </Item>
@@ -76,6 +81,6 @@ export const Menu = () => {
           </Item>
         </List>
       )}
-    </nav>
+    </Nav>
   );
 };
