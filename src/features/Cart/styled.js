bd995648cabbcbe03e5de5b692decafbf937dbb7
Michał Wodniczak <michal.wodniczak@o2.pl>
2025-02-13 18:01:25 +0100

    added Styled to Cart

--- src/features/Cart/styled.js
+++ src/features/Cart/styled.js
@@ -1,7 +1,46 @@
 import styled from "styled-components";
 
-export const CartText =  styled.p`
-    margin: 0 auto;
-    font-size:28px;
-    font-weight: 700;
+export const CartWrapper = styled.div`
+  margin: 0 auto;
+  display: flex;
+  align-items: flex-start;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.smallDevice}px) {
+    flex-direction: column;
+    align-items: center;
+  };
+`;
+
+export const CartText = styled.p`
+  margin: 0 auto;
+  font-size: 28px;
+  font-weight: 700;
+`;
+
+export const Result = styled.div`
+  padding: 20px;
+  border-radius: 5px;
+  width: 324px;
+  border: 1px solid ${({ theme }) => theme.colors.black};
+`;
+
+export const ResultTitle = styled.h2`
+  font-size: 24;
+  @media (max-width: ${({ theme }) => theme.breakpoints.smallDevice}px) {
+    font-size: 20px;
+  };
+`;
+
+export const ResultTextContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+export const ResultText = styled.p`
+  font-size: 20px;
+`;
+
+export const ResultTextValue = styled.p`
+  font-size: 24px;
+  font-weight: 700;
 `
